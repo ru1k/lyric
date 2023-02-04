89de5d5f1fdcb26188423a92e308aa8fa98f3c28
ru1k <ru1k@protonmail.com>
2023-02-05 02:48:27 +0800

    viewport

--- .index.py
+++ .index.py
@@ -5,7 +5,7 @@ def iter_link(path):
     for filename in os.listdir(path):
         if not filename.startswith('_'):
             name, ext = os.path.splitext(filename)
-            yield f'<a href="./{path}/{filename}"><h1>{name}</h1></a>'
+            yield f'<a href="./{path}/{filename}"><h2>{name}</h2></a>'
 
 
 def main():
@@ -13,15 +13,17 @@ def main():
 <html>
 
 <head>
-  <meta charset="UTF-8" />
   <title>Lyric</title>
   <style>
     div {
-      margin: auto;
       width: fit-content;
-      font-size: 2em;
+      margin: auto;
+      padding-top: 1em;
+      padding-bottom: 1em;
     }
   </style>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" />
 </head>
 
 <body>
