88a025c6491596966684526b0ce311d7d76da7d4
ru1k <ru1k@protonmail.com>
2023-02-09 04:01:18 +0800

    test

--- .index.py
+++ .index.py
@@ -3,30 +3,40 @@ from lxml import etree
 
 
 def iter_lyric(path):
+    example = load(f'{path}/.example.html')
+    title = example.find('head/title')
+    body = example.find('body')
     for filename in os.listdir(path):
         if not filename.startswith('.'):
-            filename = f'./{path}/{filename}'
-            with open(filename, 'rb') as fp:
-                html = etree.HTML(fp.read())
-            yield f'<a href="{filename}"><h2>{html.find("./head/title").text}</h2></a>'
-            dump(filename, html)
+            filename = f'{path}/{filename}'
+            html = load(filename)
+            title.text = html.find('head/title').text
+            _body = html.find('body')
+            example.replace(body, _body)
+            body = _body
+            dump(filename, example)
+            yield f'<a href="{filename}"><h2>{title.text}</h2></a>'
+
+
+def load(filename):
+    with open(filename, 'rb') as fp:
+        return etree.HTML(fp.read())
 
 
 def dump(filename, html):
     for e in html.iter():
         e.tail = None
         if e.text and e.tag not in ('h2', 'td'):
-            e.text = e.text.strip()  # if e.tag != 'style' else e.text.replace(' ', '').replace('\r', '')
-    b = etree.tostring(html, encoding='utf-8', method='html', doctype='<!DOCTYPE html>')  # .replace(b'\n', b'')
+            e.text = e.text.strip() if e.tag != 'style' else e.text.replace(' ', '').replace('\r', '')
+    b = etree.tostring(html, encoding='utf-8', method='html', doctype='<!DOCTYPE html>').replace(b'\n', b'')
     with open(filename, 'wb') as fp:
         fp.write(b)
-    return 
+    return
 
 
 def main():
     filename = 'index.html'
-    with open(filename, 'rb') as fp:
-        html = etree.HTML(fp.read())
+    html = load(filename)
     body = html.find('body')
     body.clear()
     body.append(etree.XML(f"<div>{''.join(iter_lyric('-'))}</div>"))
--- font_size.js
+++ font_size.js
@@ -1,9 +1,9 @@
-var width = document.documentElement.clientWidth;
-if (width > 980) {
-    size = 1.8;
-} else if (width > 490) {
-    size = 2.4;
-} else {
-    size = 3;
-}
-document.body.style.fontSize = size + 'em';
+// var width = document.documentElement.clientWidth;
+// if (width > 980) {
+//     size = 1.8;
+// } else if (width > 490) {
+//     size = 2.4;
+// } else {
+//     size = 3;
+// }
+// document.body.style.fontSize = size + 'em';
